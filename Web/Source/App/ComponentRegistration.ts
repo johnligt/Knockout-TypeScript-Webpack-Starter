1cfa614cd3558923804af1c85ae82118d33d145e
John Ligtenberg <john.ligtenberg@indivirtual.com>
2016-10-30 21:02:14 +0100

    Added ProductList component

--- Web/Source/App/ComponentRegistration.ts
+++ Web/Source/App/ComponentRegistration.ts
@@ -1,10 +1,12 @@
 ﻿import { MainFormComponent } from "App/Components/MainForm/MainForm";
+import { ProductListComponent } from "App/Components/ProductList/ProductList"
 
 export class ComponentRegistration {
     
     static registerComponents = () => {
 
         ko.components.register("st-main-form", new MainFormComponent());
+        ko.components.register("st-product-list", new ProductListComponent());
 
 
     }
---
+++ Web/Source/App/Components/ProductList/ProductList.ts
@@ -0,0 +1,25 @@
+﻿import { Product } from "App/Models/Product";
+import { ProductService } from "App/Services/ProductService";
+
+export class ProductListViewModel {
+
+    products: Product[];
+
+    constructor(params) {
+
+        this.products = ProductService.productList;
+
+    }
+
+}
+
+export class ProductListComponent {
+
+    constructor() {
+
+        return {
+            viewModel: ProductListViewModel,
+            template: require("text!./ProductListView.html")
+        }
+    }
+}
