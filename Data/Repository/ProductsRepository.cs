a8b36045b8f8fccedcb13179fb643e0987904389
John Ligtenberg <john.ligtenberg@indivirtual.com>
2016-10-30 20:43:58 +0100

    Cleanup

--- Data/Repository/ProductsRepository.cs
+++ Data/Repository/ProductsRepository.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Data.Helpers;
 using Data.Models;
 using Newtonsoft.Json;
@@ -10,7 +8,7 @@ namespace Data.Repository
 {
     public static class ProductsRepository
     {
-        public static List<Product> GetProducts()
+        public static IEnumerable<Product> GetProducts()  
         {
             //This is where you would get your data from a database.
             //To keep this project simple, the data are loaded from JSON files
