e198a9fa963f0c2d1953ccca0c67c9dee4dea3f5
John Ligtenberg <john.ligtenberg@indivirtual.com>
2016-11-03 08:05:05 +0100

    Added shopping bag component

--- Web/Source/App/ComponentRegistration.ts
+++ Web/Source/App/ComponentRegistration.ts
@@ -1,5 +1,6 @@
 ﻿import { MainFormComponent } from "App/MainForm/MainForm";
 import { ProductListComponent } from "App/Components/ProductList/ProductList";
+import { ShoppingBagComponent } from "App/Components/ShoppingBag/ShoppingBag";
 import { FormStepSelectProductsComponent } from "App/FormSteps/FormStepSelectProducts/FormStepSelectProducts";
 import { FormStepPersonalDetailsComponent } from "App/FormSteps/FormStepPersonalDetails/FormStepPersonalDetails";
 import { FormStepCheckComponent } from "App/FormSteps/FormStepCheck/FormStepCheck";
@@ -8,6 +9,9 @@ import { NavigationComponent } from "App/FormSteps/Navigation/Navigation";
 export class ComponentRegistration {
     
     static registerComponents = () => {
+
+        //MainForm, contains all other components, either directly or as nested components.
+        ko.components.register("st-main-form", new MainFormComponent());
 
         //FormSteps
         ko.components.register("st-formstep-selectproducts", new FormStepSelectProductsComponent());
@@ -18,8 +22,8 @@ export class ComponentRegistration {
         ko.components.register("st-navigation", new NavigationComponent());
 
         //Components
-        ko.components.register("st-main-form", new MainFormComponent());
         ko.components.register("st-product-list", new ProductListComponent());
+        ko.components.register("st-shopping-bag", new ShoppingBagComponent());
 
 
     }
---
+++ Web/Source/App/Components/ShoppingBag/ShoppingBag.ts
@@ -0,0 +1,18 @@
+﻿export class ShoppingBagViewModel {
+
+    constructor(params) {
+        
+    }
+
+}
+
+export class ShoppingBagComponent {
+
+    constructor() {
+
+        return {
+            viewModel: ShoppingBagViewModel,
+            template: require("text!./ShoppingBagView.html")
+        }
+    }
+}
